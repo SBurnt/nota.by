da6a16fb778bce49f3a4c203504551e054c07c00
SBurnt <zip2004zip@gmail.com>
2020-10-20 00:36:59 +0300

    feat: add button show more advantages at <1024px

--- assets/site/js/custom.js
+++ assets/site/js/custom.js
@@ -1,53 +1,73 @@
-const sliderAbout = document.querySelector('.js-about-slider');
-let swiper = new Swiper(sliderAbout, {
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next.slider__arrows-next',
-    prevEl: '.swiper-button-prev.slider__arrows-prev',
-  },
-});
+$(document).ready(function () {
+  const sliderAbout = document.querySelector('.js-about-slider');
+  let swiper = new Swiper(sliderAbout, {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next.slider__arrows-next',
+      prevEl: '.swiper-button-prev.slider__arrows-prev',
+    },
+  });
 
-const sliderOurTeam = document.querySelector('.js-our-team-slider');
-let swiper2 = new Swiper(sliderOurTeam, {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  centeredSlides: true,
-  loop: true,
-  navigation: {
-    nextEl: '.our-team .swiper-button-next',
-    prevEl: '.our-team .swiper-button-prev',
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-      centeredSlides: false,
+  const sliderOurTeam = document.querySelector('.js-our-team-slider');
+  let swiper2 = new Swiper(sliderOurTeam, {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: '.our-team .swiper-button-next',
+      prevEl: '.our-team .swiper-button-prev',
     },
-    768: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        centeredSlides: false,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
     },
-  },
-});
+  });
 
-const sliderReviews = document.querySelector('.js-reviews-slider');
-let swiper3 = new Swiper(sliderReviews, {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  // centeredSlides: true,
-  loop: true,
-  navigation: {
-    nextEl: '.reviews .swiper-button-next',
-    prevEl: '.reviews .swiper-button-prev',
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+  const sliderReviews = document.querySelector('.js-reviews-slider');
+  let swiper3 = new Swiper(sliderReviews, {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    // centeredSlides: true,
+    loop: true,
+    navigation: {
+      nextEl: '.reviews .swiper-button-next',
+      prevEl: '.reviews .swiper-button-prev',
     },
-    // 768: {
-    //   slidesPerView: 1,
-    //   spaceBetween: 10,
-    // },
-  },
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // 768: {
+      //   slidesPerView: 1,
+      //   spaceBetween: 10,
+      // },
+    },
+  });
+
+  const btnMoreAdvantages = $('.js-advantages__btn-more');
+  if (btnMoreAdvantages.length) {
+    let advantagesItem = $('.advantages__item');
+    // let advantagesItemVis = $('.advantages__item--visible');
+
+    btnMoreAdvantages.on('click', function () {
+      // advantagesItemVis.slideToggle();
+      if (advantagesItem.hasClass('advantages__item--visible')) {
+        advantagesItem.removeClass('advantages__item--visible');
+      } else {
+        advantagesItem.addClass('advantages__item--visible');
+      }
+      $(this).html(
+        advantagesItem.hasClass('advantages__item--visible') ? 'свернуть преимущества' : 'развернуть преимущества'
+      );
+    });
+  }
 });
