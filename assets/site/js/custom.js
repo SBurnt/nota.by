394169d07d0477821d4284204fbbb9ea21bf9c95
SBurnt <zip2004zip@gmail.com>
2020-10-20 15:20:05 +0300

    func appearance of info on scroll, click team sect

--- assets/site/js/custom.js
+++ assets/site/js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // главный слайдер на странице brief
   const sliderAbout = document.querySelector('.js-about-slider');
   let swiper = new Swiper(sliderAbout, {
     loop: true,
@@ -8,6 +9,7 @@ $(document).ready(function () {
     },
   });
 
+  // слайдер наша команда на странице brief
   const sliderOurTeam = document.querySelector('.js-our-team-slider');
   let swiper2 = new Swiper(sliderOurTeam, {
     slidesPerView: 3,
@@ -31,6 +33,7 @@ $(document).ready(function () {
     },
   });
 
+  // слайдер отзывы на странице brief
   const sliderReviews = document.querySelector('.js-reviews-slider');
   let swiper3 = new Swiper(sliderReviews, {
     slidesPerView: 2,
@@ -53,6 +56,7 @@ $(document).ready(function () {
     },
   });
 
+  // показать больше в секции преимущества на странице brief START
   const btnMoreAdvantages = $('.js-advantages__btn-more');
   if (btnMoreAdvantages.length) {
     let advantagesItem = $('.advantages__item');
@@ -70,4 +74,40 @@ $(document).ready(function () {
       );
     });
   }
+  // показать больше в секции преимущества на странице brief END
+
+  // появление инфомации при скролле и клике(мобилка) о человеке в секции наша команда на странице brief START
+  const ourTeamCard = document.querySelectorAll('.our-team__card');
+  const ourTeamCardActiv = document.querySelector('.our-team__card.swiper-slide-active .our-team__card-info');
+  let ourTeamCardActivInfoShow = false;
+
+  function showOurTeamCardActivInfo(el) {
+    el.classList.add('scroll');
+    ourTeamCardActivInfoShow = true;
+    setTimeout(function () {
+      el.classList.remove('scroll');
+    }, 800);
+  }
+
+  // появление при скролле
+  if (ourTeamCard.length) {
+    window.addEventListener('scroll', function () {
+      ourTeamCard.forEach(function (el, i) {
+        if (
+          el.getBoundingClientRect().top <= document.documentElement.clientHeight / 2 &&
+          ourTeamCardActivInfoShow == false
+        ) {
+          showOurTeamCardActivInfo(ourTeamCardActiv);
+        }
+      });
+    });
+  }
+
+  // появление при клике на мобилке
+  ourTeamCard.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      showOurTeamCardActivInfo(e.target.nextElementSibling);
+    });
+  });
+  // появление инфомации о человеке секции наша команда на странице brief END
 });
