ab2c470ecbeafa474e30c5e9a26a7a170ffb129a
SBurnt <zip2004zip@gmail.com>
2021-01-26 22:27:38 +0300

    anchor when clicking next

--- assets/site/js/custom.js
+++ assets/site/js/custom.js
@@ -235,4 +235,15 @@ $(document).ready(function () {
       .animate({ scrollTop: $(d).offset().top }, t);
   });
   // переход к квизу по якорю END
+
+  // переход к квизу по якорю при клике "следующий шаг" START
+  $('a[href^="#quiz-next"], *[data-href^="#quiz-next"]').on('click', function (e) {
+    e.preventDefault();
+    var t = 300;
+    var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+    $('html,body')
+      .stop()
+      .animate({ scrollTop: $(d).offset().top }, t);
+  });
+  // переход к квизу по якорю при клике "следующий шаг" END
 });
